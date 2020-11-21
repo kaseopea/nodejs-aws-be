5f624211dae2f6a530f3834e27336ad183695d08
Vitaly <kaseopea@gmail.com>
2020-11-21 15:39:47 +0300

    Task 6

---
+++ src/product-service/catalogBatchProcess.js
@@ -0,0 +1,60 @@
+import AWS from 'aws-sdk';
+import { productService } from './services/product.service';
+import { responseService } from '../libs/services/response.service';
+import { errorService } from '../libs/services/error.service';
+import { GENERAL_CONFIG } from '../config/genaral.config';
+
+const sns = new AWS.SNS({
+    apiVersion: '2010-03-31',
+    region: GENERAL_CONFIG.region,
+});
+
+const publishMessage = async (message) => {
+    return new Promise((resolve, reject) => {
+        sns.publish({
+            Subject: 'CatalogBatchProcess Event',
+            Message: JSON.stringify(message),
+            TopicArn: process.env.SNS_ARN
+        }, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+};
+
+export const handler = async (event) => {
+    // create product in DB
+    for (const record of event.Records) {
+        const item = JSON.parse(record.body);
+        try {
+            if (!item.title || (typeof item.title !== 'string')) {
+                throw {
+                    status: 400,
+                    message: '400 Invalid payload'
+                };
+            }
+            const product = await productService.createProduct(item);
+            if (!product) {
+                throw {
+                    status: 404,
+                    message: '404 Product not found'
+                };
+            }
+            console.log('######', product);
+            // SNS
+            const message = await publishMessage(product);
+            console.log(message);
+
+            response = responseService.getResponse(200, product);
+    
+        } catch(err) {
+            response = errorService.handleError(err);
+        }
+
+        return await Promise.resolve(response);
+    }
+    
+}
--- src/product-service/welcome.js
+++ src/product-service/welcome.js
@@ -2,7 +2,18 @@ import { responseService } from '../libs/services/response.service';
 
 export const handler = async (event) => {
     const message = 'Mess with the Best, Die like the Rest';
-    const response = responseService.getResponsePlain(200, message);
+    console.log({
+        sqaUrl: process.env.SQS_URL,
+        sqaArn: process.env.SQS_ARN,
+        message,
+    });
+    const response = responseService.getResponsePlain(200, {
+        sqaUrl: process.env.SQS_URL,
+        sqaArn: process.env.SQS_ARN,
+        message,
+    });
+
+    console.log(JSON.stringify(response));
 
     return await Promise.resolve(response);
 }
