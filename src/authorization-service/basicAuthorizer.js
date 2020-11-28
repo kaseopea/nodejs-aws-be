5962930dc4204f7a2b9da2df146980450e3ee675
Vitali_Rusau <Vitali_Rusau@epam.com>
2020-11-28 23:20:12 +0300

    Task 7

--- src/authorization-service/basicAuthorizer.js
+++ src/authorization-service/basicAuthorizer.js
@@ -1,2 +1,43 @@
-export const handler = async (event) => {
+const generatePolicy = (principalId, resource, effect = 'Deny') => {
+    return {
+        principalId,
+        policyDocument: {
+            Version: '2012-10-17',
+            Statement: [
+                {
+                    Action: 'execute-api:Invoke',
+                    Effect: effect,
+                    Resource: resource
+                }
+            ]
+        }
+    };
+};
+
+export const handler = async (event, ctx, cb) => {
+    console.log('Event: ', JSON.stringify(event));
+    
+    if (event['type'] !== 'REQUEST') {
+        cb('Unauthorized');
+    }
+
+    try {
+        const authorizationToken = event['headers']['Authorization'];
+        const encodedCreds = authorizationToken.split(' ')[1];
+        const buff = Buffer.from(encodedCreds, 'base64');
+        const plainCreds = buff.toString('utf-8').split(':');
+        const username = plainCreds[0];
+        const password = plainCreds[1];
+
+        console.log(`Username: ${username} | Password: ${password}`);
+
+        const storedPwd = process.env[username];
+        const effect = (!storedPwd || storedPwd !== password) ? 'Deny': 'Allow';
+        const policy = generatePolicy(encodedCreds, event.methodArn, effect);
+
+        cb(null, policy);
+
+    } catch(e) {
+        cb(`Unauthorized: ${e.message}`);
+    }
 }
